bc01ddc5dc272fa8eb42c130f013ab4daed9096f
Cagri Yolyapar <cagri.yolyapar@gmail.com>
2022-04-18 11:37:20 +0300

    CagriController kok dizindeki Controller'a eklendi

---
+++ MVCCollab_1/Controllers/CagriController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVCCollab_1.Controllers
+{
+    public class CagriController : Controller
+    {
+        // GET: Cagri
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
