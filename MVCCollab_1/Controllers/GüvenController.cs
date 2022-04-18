6c4830854e011eddc111c7c49c553ce6c3f62b7b
güven boydak <gvn.boydak@gmail.com>
2022-04-18 11:50:52 +0300

    Güven controller eklendi.

---
+++ MVCCollab_1/Controllers/GüvenController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVCCollab_1.Controllers
+{
+    public class GüvenController : Controller
+    {
+        // GET: Güven
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
